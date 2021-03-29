4efc6ba6c6a691f703b8be3105f84f14b53351f3
phhad <philippe.hadjis@polymtl.ca>
2021-03-29 15:26:50 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -10,14 +10,17 @@ import numpy as np
 
 
 def linear_values() -> np.ndarray:
-    return np.linspace(start=-1.3, stop=2.5, number=64)
+    return np.linspace(-1.3, 2.5, 64)
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
+    cartesian_coordinates
     return np.array([])
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
+    values = np.ndarray([3,2,5])
+    number = 1
     return 0
 
 
